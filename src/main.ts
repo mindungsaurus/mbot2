97f6dce9591ae7d2231bbb57a4de7fc82134b7ba
ming <mindu04879@gmail.com>
2025-08-25 00:28:01 +0900

    로그 추가
    하이든아님

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log(process.env.DISCORD_BOT_TOKEN);
   console.log('DATABASE_URL at runtime:', process.env.DATABASE_URL);
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);

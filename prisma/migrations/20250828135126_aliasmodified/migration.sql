549a759be062da5f7096acc0fe56fab7930cdebd
ming <mindu04879@gmail.com>
2025-09-01 05:49:22 +0900

    좀 돼라
    아오

--- prisma/migrations/20250828135126_aliasmodified/migration.sql
+++ prisma/migrations/20250828135126_aliasmodified/migration.sql
@@ -6,18 +6,3 @@
 */
 -- CreateIndex
 CREATE UNIQUE INDEX "ItemAlias_alias_key" ON "public"."ItemAlias"("alias");
-
--- Create ko-KR collation if not exists
-DO $$
-BEGIN
-  IF NOT EXISTS (SELECT 1 FROM pg_collation WHERE collname = 'ko_kr') THEN
-    CREATE COLLATION "ko_kr" (provider = icu, locale = 'ko-KR');
-  END IF;
-END $$;
-
--- Apply collation to column
-ALTER TABLE "ItemsInfo"
-  ALTER COLUMN "name" TYPE text COLLATE "ko_kr";
-
-ALTER TABLE "ItemAlias"
-  ALTER COLUMN "alias" TYPE text COLLATE "ko_kr";
